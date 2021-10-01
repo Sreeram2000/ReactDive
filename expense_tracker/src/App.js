f2af111041ebd4ddff045b45b65649ade45bb1e4
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-10-02 03:13:29 +0530

    creating and passing props

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,13 +1,30 @@
 import './App.css';
 import Expenses from './components/Expenses'
 function App() {
-  const props = {
+  const expense = [
+    {
+      date : 15,
+      title: "Insurance",
+      cost : 50
+    },
+    {
+      date : 17,
+      title: "Eat_Out",
+      cost : 199
+    },
+    {
+      date : 21,
+      title: "Picnic",
+      cost : 105
+    }
+  ]
 
-  }
   return (
     <div className="App">
       <h2>Hello! Lets See How You Did This Month..</h2>
-      <Expenses />
+      <Expenses expenses={expense[0]} />
+      <Expenses expenses={expense[1]} />
+      <Expenses expenses={expense[2]} />
     </div>
   );
 }
--- expense_tracker/src/components/Expenses.js
+++ expense_tracker/src/components/Expenses.js
@@ -3,24 +3,9 @@ function Expenses(props) {
     return (
         <div className="Expenses-container">
             <div className="Expenses-item">
-                <div className="Expenses-date">date</div>
-                <div className="Expenses-title">Eat_Out</div>
-                <div className="Expenses-cost">$191</div>
-            </div>
-            <div className="Expenses-item">
-                <div className="Expenses-date">date</div>
-                <div className="Expenses-title">Eat_Out</div>
-                <div className="Expenses-cost">$191</div>
-            </div>
-            <div className="Expenses-item">
-                <div className="Expenses-date">date</div>
-                <div className="Expenses-title">Eat_Out</div>
-                <div className="Expenses-cost">$191</div>
-            </div>
-            <div className="Expenses-item">
-                <div className="Expenses-date">date</div>
-                <div className="Expenses-title">Eat_Out</div>
-                <div className="Expenses-cost">$191</div>
+                <div className="Expenses-date">{props.expenses.date}</div>
+                <div className="Expenses-title">{props.expenses.title}</div>
+                <div className="Expenses-cost">${props.expenses.cost}</div>
             </div>
         </div>
     )
