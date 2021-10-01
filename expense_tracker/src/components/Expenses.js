985d9375eb16ae8db2f004e7b05f4b3a67b9f2aa
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-10-02 03:30:04 +0530

    Add UseState to title

--- expense_tracker/src/components/Expenses.js
+++ expense_tracker/src/components/Expenses.js
@@ -1,11 +1,20 @@
+import React,{useState} from 'react';
 import './Expenses.css';
 function Expenses(props) {
+    // let title = props.expenses.title
+    let [title,setTitle] = useState(props.expenses.title)
     return (
         <div className="Expenses-container">
             <div className="Expenses-item">
                 <div className="Expenses-date">{props.expenses.date}</div>
-                <div className="Expenses-title">{props.expenses.title}</div>
+                <div className="Expenses-title">{title}</div>
                 <div className="Expenses-cost">${props.expenses.cost}</div>
+                <button onClick={ () => 
+                    {
+                        setTitle('Updated!')
+                        console.log("Hurray!!")
+                    }
+                }>Update</button>
             </div>
         </div>
     )
