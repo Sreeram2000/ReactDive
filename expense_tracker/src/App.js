9f57f832f66d0d86f0f7718f0aa0519165f83a81
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-10-01 23:26:02 +0530

    edit Expenses.js Expenses.css

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import Expenses from './components/Expenses'
 function App() {
   return (
     <div className="App">
-      <h2>Building an ExpenseTracker App</h2>
+      <h2>Hello!</h2>
+      <Expenses />
     </div>
   );
 }
---
+++ expense_tracker/src/components/Expenses.js
@@ -0,0 +1,24 @@
+import './Expenses.css';
+function Expenses() {
+    return (
+        <div className="Expenses-container">
+            <div className="Expenses-item">
+                <div>09 sept 2021</div>
+                <div>Eat_Out</div>
+                <div>$191</div>
+            </div>
+            <div className="Expenses-item">
+                <div>09 sept 2021</div>
+                <div>Eat_Out</div>
+                <div>$191</div>
+            </div>
+            <div className="Expenses-item">
+                <div>09 sept 2021</div>
+                <div>Eat_Out</div>
+                <div>$191</div>
+            </div>
+        </div>
+    )
+}
+
+export default Expenses
