ff4b486220329c5efced18009262d8461001a2b5
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-10-10 04:20:43 +0530

    send props to fetch form

--- name_Dob/src/components/PageForm.js
+++ name_Dob/src/components/PageForm.js
@@ -1,14 +1,30 @@
+import React,{useState} from 'react'
 import './PageForm.css' 
-function PageForm() {
+function PageForm(props) {
+    const [Name, setName] = useState("")
+    const [Age, setAge] = useState("")
+    function nameHandler(event) {
+        setName(event.target.value)
+    }
+    function ageHandler(event) {
+        setAge(event.target.value)
+    }
+
+    function onHandler(event) {
+        event.preventDefault()
+        props.onSubmitHandler(Name,Age)
+        setName("")
+        setAge("")
+    }
     return (
-        <form className="main-container"> 
+        <form className="main-container" onSubmit={onHandler}> 
             <div className="main-form">
                 <label htmlFor="name">Name</label>
-                <input type="text" />
+                <input type="text"  onChange={nameHandler}/>
             </div>
             <div className="main-form">
-                <label htmlFor="Age">Age(In years)</label>
-                <input type="text" />
+                <label htmlFor="Age"></label>
+                <input type="text"  onChange={ageHandler}/>
             </div>
             <div className="main-form">
                 <button type="submit">Update</button>
