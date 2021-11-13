3a9b32427f0400d11b711ee29f6524cd239d2685
Sreeram Shenai <sreeramshenai2000@gmail.com>
2021-11-13 22:21:14 +0530

    Update Index.js

--- name_Dob/src/index.js
+++ name_Dob/src/index.js
@@ -1,3 +1,16 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import './index.css';
+// import App from './App';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
